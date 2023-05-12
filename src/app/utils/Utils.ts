41472f1cca4d726513ac964ab612afb86b892c6f
Vlad Horovyi <vladyslav.horovyi@globallogic.com>
2023-05-13 01:42:51 +0300

    feature(game-of-life): edges now calculating due to diff equations for n-metric space

--- src/app/utils/Utils.ts
+++ src/app/utils/Utils.ts
@@ -7,6 +7,119 @@ export class Utils {
     colIndex: number
   ): number {
     let numberOfAliveNeighbors = 0;
+
+    if (rowIndex === 0) {
+      if (colIndex === 0) {
+        cells?.[cells.length - 1]?.[colIndex]
+          ? (numberOfAliveNeighbors += 1)
+          : null;
+        cells?.[rowIndex]?.[colIndex + 1]
+          ? (numberOfAliveNeighbors += 1)
+          : null;
+        cells?.[rowIndex + 1]?.[colIndex]
+          ? (numberOfAliveNeighbors += 1)
+          : null;
+        cells?.[rowIndex]?.[cells?.[rowIndex].length - 1]
+          ? (numberOfAliveNeighbors += 1)
+          : null;
+        cells?.[cells.length - 1]?.[cells?.[cells.length - 1].length - 1]
+          ? (numberOfAliveNeighbors += 1)
+          : null;
+        cells?.[cells.length - 1]?.[colIndex + 1]
+          ? (numberOfAliveNeighbors += 1)
+          : null;
+        cells?.[rowIndex + 1]?.[cells?.[rowIndex + 1].length - 1]
+          ? (numberOfAliveNeighbors += 1)
+          : null;
+        cells?.[rowIndex + 1]?.[colIndex + 1]
+          ? (numberOfAliveNeighbors += 1)
+          : null;
+        return numberOfAliveNeighbors;
+      }
+
+      if (colIndex === cells[rowIndex].length - 1) {
+        cells?.[cells.length - 1]?.[colIndex]
+          ? (numberOfAliveNeighbors += 1)
+          : null;
+        cells?.[rowIndex]?.[0] ? (numberOfAliveNeighbors += 1) : null;
+        cells?.[rowIndex + 1]?.[colIndex]
+          ? (numberOfAliveNeighbors += 1)
+          : null;
+        cells?.[rowIndex]?.[colIndex - 1]
+          ? (numberOfAliveNeighbors += 1)
+          : null;
+        cells?.[cells.length - 1]?.[colIndex - 1]
+          ? (numberOfAliveNeighbors += 1)
+          : null;
+        cells?.[cells.length - 1]?.[0] ? (numberOfAliveNeighbors += 1) : null;
+        cells?.[rowIndex + 1]?.[colIndex - 1]
+          ? (numberOfAliveNeighbors += 1)
+          : null;
+        cells?.[rowIndex + 1]?.[0] ? (numberOfAliveNeighbors += 1) : null;
+        return numberOfAliveNeighbors;
+      }
+
+      cells?.[cells.length - 1]?.[colIndex]
+        ? (numberOfAliveNeighbors += 1)
+        : null;
+      cells?.[rowIndex]?.[colIndex + 1] ? (numberOfAliveNeighbors += 1) : null;
+      cells?.[rowIndex + 1]?.[colIndex] ? (numberOfAliveNeighbors += 1) : null;
+      cells?.[rowIndex]?.[colIndex - 1] ? (numberOfAliveNeighbors += 1) : null;
+      cells?.[cells.length - 1]?.[colIndex - 1]
+        ? (numberOfAliveNeighbors += 1)
+        : null;
+      cells?.[cells.length - 1]?.[colIndex + 1]
+        ? (numberOfAliveNeighbors += 1)
+        : null;
+      cells?.[rowIndex + 1]?.[colIndex - 1]
+        ? (numberOfAliveNeighbors += 1)
+        : null;
+      cells?.[rowIndex + 1]?.[colIndex + 1]
+        ? (numberOfAliveNeighbors += 1)
+        : null;
+      return numberOfAliveNeighbors;
+    }
+
+    if (colIndex === 0) {
+      cells?.[rowIndex - 1]?.[colIndex] ? (numberOfAliveNeighbors += 1) : null;
+      cells?.[rowIndex]?.[colIndex + 1] ? (numberOfAliveNeighbors += 1) : null;
+      cells?.[rowIndex + 1]?.[colIndex] ? (numberOfAliveNeighbors += 1) : null;
+      cells?.[rowIndex]?.[cells?.[rowIndex].length - 1]
+        ? (numberOfAliveNeighbors += 1)
+        : null;
+      cells?.[rowIndex - 1]?.[cells?.[rowIndex].length - 1]
+        ? (numberOfAliveNeighbors += 1)
+        : null;
+      cells?.[rowIndex - 1]?.[colIndex + 1]
+        ? (numberOfAliveNeighbors += 1)
+        : null;
+      cells?.[rowIndex + 1]?.[cells?.[rowIndex].length - 1]
+        ? (numberOfAliveNeighbors += 1)
+        : null;
+      cells?.[rowIndex + 1]?.[colIndex + 1]
+        ? (numberOfAliveNeighbors += 1)
+        : null;
+
+      return numberOfAliveNeighbors;
+    }
+
+    if (colIndex === cells[rowIndex].length - 1) {
+      cells?.[rowIndex - 1]?.[colIndex] ? (numberOfAliveNeighbors += 1) : null;
+      cells?.[rowIndex]?.[0] ? (numberOfAliveNeighbors += 1) : null;
+      cells?.[rowIndex + 1]?.[colIndex] ? (numberOfAliveNeighbors += 1) : null;
+      cells?.[rowIndex]?.[colIndex - 1] ? (numberOfAliveNeighbors += 1) : null;
+      cells?.[rowIndex - 1]?.[colIndex - 1]
+        ? (numberOfAliveNeighbors += 1)
+        : null;
+      cells?.[rowIndex - 1]?.[0] ? (numberOfAliveNeighbors += 1) : null;
+      cells?.[rowIndex + 1]?.[colIndex - 1]
+        ? (numberOfAliveNeighbors += 1)
+        : null;
+      cells?.[rowIndex + 1]?.[0] ? (numberOfAliveNeighbors += 1) : null;
+
+      return numberOfAliveNeighbors;
+    }
+
     cells?.[rowIndex - 1]?.[colIndex] ? (numberOfAliveNeighbors += 1) : null;
     cells?.[rowIndex]?.[colIndex + 1] ? (numberOfAliveNeighbors += 1) : null;
     cells?.[rowIndex + 1]?.[colIndex] ? (numberOfAliveNeighbors += 1) : null;
@@ -33,6 +146,111 @@ export class Utils {
     colIndex: number
   ): number {
     let numberOfAliveNeighbors = 0;
+
+    if (rowIndex === 0) {
+      if (colIndex === 0) {
+        cells?.[cells.length - 1]?.[colIndex]
+          ? (numberOfAliveNeighbors += 1)
+          : null;
+        cells?.[rowIndex]?.[colIndex + 1]
+          ? (numberOfAliveNeighbors += 1)
+          : null;
+        cells?.[rowIndex + 1]?.[colIndex]
+          ? (numberOfAliveNeighbors += 1)
+          : null;
+        cells?.[rowIndex]?.[cells?.[rowIndex].length - 1]
+          ? (numberOfAliveNeighbors += 1)
+          : null;
+        cells?.[rowIndex + 2]?.[colIndex]
+          ? (numberOfAliveNeighbors += 1)
+          : null;
+        cells?.[rowIndex]?.[colIndex + 2]
+          ? (numberOfAliveNeighbors += 1)
+          : null;
+        cells?.[cells.length - 2]?.[colIndex]
+          ? (numberOfAliveNeighbors += 1)
+          : null;
+        cells?.[rowIndex]?.[cells?.[rowIndex].length - 2]
+          ? (numberOfAliveNeighbors += 1)
+          : null;
+        return numberOfAliveNeighbors;
+      }
+
+      if (colIndex === cells[rowIndex].length - 1) {
+        cells?.[cells.length - 1]?.[colIndex]
+          ? (numberOfAliveNeighbors += 1)
+          : null;
+        cells?.[rowIndex]?.[cells?.[rowIndex].length - 1]
+          ? (numberOfAliveNeighbors += 1)
+          : null;
+        cells?.[rowIndex + 1]?.[colIndex]
+          ? (numberOfAliveNeighbors += 1)
+          : null;
+        cells?.[rowIndex]?.[colIndex - 1]
+          ? (numberOfAliveNeighbors += 1)
+          : null;
+        cells?.[rowIndex + 2]?.[colIndex]
+          ? (numberOfAliveNeighbors += 1)
+          : null;
+        cells?.[rowIndex]?.[cells?.[rowIndex].length - 2]
+          ? (numberOfAliveNeighbors += 1)
+          : null;
+        cells?.[cells.length - 2]?.[colIndex]
+          ? (numberOfAliveNeighbors += 1)
+          : null;
+        cells?.[rowIndex]?.[colIndex - 2]
+          ? (numberOfAliveNeighbors += 1)
+          : null;
+
+        return numberOfAliveNeighbors;
+      }
+
+      cells?.[cells.length - 1]?.[colIndex]
+        ? (numberOfAliveNeighbors += 1)
+        : null;
+      cells?.[rowIndex]?.[colIndex + 1] ? (numberOfAliveNeighbors += 1) : null;
+      cells?.[rowIndex + 1]?.[colIndex] ? (numberOfAliveNeighbors += 1) : null;
+      cells?.[rowIndex]?.[colIndex - 1] ? (numberOfAliveNeighbors += 1) : null;
+      cells?.[rowIndex + 2]?.[colIndex] ? (numberOfAliveNeighbors += 1) : null;
+      cells?.[rowIndex]?.[colIndex + 2] ? (numberOfAliveNeighbors += 1) : null;
+      cells?.[cells.length - 2]?.[colIndex]
+        ? (numberOfAliveNeighbors += 1)
+        : null;
+      cells?.[rowIndex]?.[colIndex - 2] ? (numberOfAliveNeighbors += 1) : null;
+
+      return numberOfAliveNeighbors;
+    }
+
+    if (colIndex === 0) {
+      cells?.[rowIndex - 1]?.[colIndex] ? (numberOfAliveNeighbors += 1) : null;
+      cells?.[rowIndex]?.[colIndex + 1] ? (numberOfAliveNeighbors += 1) : null;
+      cells?.[rowIndex + 1]?.[colIndex] ? (numberOfAliveNeighbors += 1) : null;
+      cells?.[rowIndex]?.[cells?.[rowIndex].length - 1]
+        ? (numberOfAliveNeighbors += 1)
+        : null;
+      cells?.[rowIndex + 2]?.[colIndex] ? (numberOfAliveNeighbors += 1) : null;
+      cells?.[rowIndex]?.[colIndex + 2] ? (numberOfAliveNeighbors += 1) : null;
+      cells?.[rowIndex - 2]?.[colIndex] ? (numberOfAliveNeighbors += 1) : null;
+      cells?.[rowIndex]?.[cells?.[rowIndex].length - 2]
+        ? (numberOfAliveNeighbors += 1)
+        : null;
+
+      return numberOfAliveNeighbors;
+    }
+
+    if (colIndex === cells[rowIndex].length - 1) {
+      cells?.[rowIndex - 1]?.[colIndex] ? (numberOfAliveNeighbors += 1) : null;
+      cells?.[rowIndex]?.[0] ? (numberOfAliveNeighbors += 1) : null;
+      cells?.[rowIndex + 1]?.[colIndex] ? (numberOfAliveNeighbors += 1) : null;
+      cells?.[rowIndex]?.[colIndex - 1] ? (numberOfAliveNeighbors += 1) : null;
+      cells?.[rowIndex + 2]?.[colIndex] ? (numberOfAliveNeighbors += 1) : null;
+      cells?.[rowIndex]?.[1] ? (numberOfAliveNeighbors += 1) : null;
+      cells?.[rowIndex - 2]?.[colIndex] ? (numberOfAliveNeighbors += 1) : null;
+      cells?.[rowIndex]?.[colIndex - 2] ? (numberOfAliveNeighbors += 1) : null;
+
+      return numberOfAliveNeighbors;
+    }
+
     cells?.[rowIndex - 1]?.[colIndex] ? (numberOfAliveNeighbors += 1) : null;
     cells?.[rowIndex]?.[colIndex + 1] ? (numberOfAliveNeighbors += 1) : null;
     cells?.[rowIndex + 1]?.[colIndex] ? (numberOfAliveNeighbors += 1) : null;
